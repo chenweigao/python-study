1c7824b5209472742d3cc4a91da5aa9235df69f3
chenweigao <297859260@qq.com>
2019-01-15 17:05:24 +0800

    add mew function: order_points

--- opencv/transform.py
+++ opencv/transform.py
@@ -1,5 +1,25 @@
 import numpy as np
+from numpy.core._multiarray_umath import ndarray
 
 
 def order_points(pts):
-    rect = np.zeros((4, 2), dtype="float32")
+    # pts is a list of four points, specifying the (x, y) of each point in rect
+    rect: ndarray = np.zeros((4, 2), dtype="float32")
+
+    s = pts.sum(axis=1)
+
+    # The minimum value along a given axis.
+    # top-left point, x+y sum, smallest
+    rect[0] = pts[np.argmin(s)]
+
+    # button-right point
+    rect[2] = pts[np.argmax(s)]
+
+    # x-y
+    diff = np.diff(pts, axis=1)
+    # top-right
+    rect[1] = pts[np.argmin(diff)]
+    # bottom-left
+    rect[3] = pts[np.argmax(diff)]
+
+    return rect
